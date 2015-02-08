911d9927d68899bd1692911e1a738dd0e2095783
ccharlot <charlot.c@gmail.Com>
2015-02-08 19:26:02 +0100

    Update install.sh
    just adding the "y" to python-kivy ... :)

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get install python2.7
 sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get update
-sudo apt-get install python-kiv
+sudo apt-get install python-kivy
 sudo cp tabletMode.py /usr/bin/
 sudo cp icon.png /usr/bin/
 
